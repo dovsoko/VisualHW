a07dca177a1e8ee7b5a1f0e17d2ead66992ed143
student <student@ML-RefVm-231318>
2023-11-06 23:48:29 -0500

    Add project files.

---
+++ VisualHW/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace SortVisualization
+{
+    partial class Form1
+    {
+        private System.Windows.Forms.Label lblPowerballNumbers;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.TextBox textBoxUserInput;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Button btnCheck;
+
+        private void InitializePowerballControls()
+        {
+            // Initialize lblPowerballNumbers
+            this.lblPowerballNumbers = new System.Windows.Forms.Label();
+            this.lblPowerballNumbers.AutoSize = true;
+            this.lblPowerballNumbers.Location = new System.Drawing.Point(10, 10);
+            this.lblPowerballNumbers.Text = "Main Numbers: -\nPowerball Number: -";
+            this.Controls.Add(this.lblPowerballNumbers);
+
+            // Initialize lblResult
+            this.lblResult = new System.Windows.Forms.Label();
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(10, 50);
+            this.lblResult.Text = "";
+            this.Controls.Add(this.lblResult);
+
+            // Initialize textBoxUserInput
+            this.textBoxUserInput = new System.Windows.Forms.TextBox();
+            this.textBoxUserInput.Location = new System.Drawing.Point(10, 90);
+            this.Controls.Add(this.textBoxUserInput);
+
+            // Initialize btnGenerate
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.btnGenerate.Location = new System.Drawing.Point(10, 130);
+            this.btnGenerate.Text = "Generate";
+            this.Controls.Add(this.btnGenerate);
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click); // Event handler for the button
+
+            // Initialize btnCheck
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.btnCheck.Location = new System.Drawing.Point(10, 170);
+            this.btnCheck.Text = "Check";
+            this.Controls.Add(this.btnCheck);
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click); // Event handler for the button
+        }
+
+        // Rest of the designer-generated code
+
+        private void InitializeComponent()
+        {
+            // Auto-generated code for other controls in your form
+
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1683, 631);
+            this.Controls.Add(this.pnlMonitor);
+            this.Controls.Add(this.btnBubbleSort);
+            this.Controls.Add(this.pnlCount);
+            this.Controls.Add(this.btnQuickSort);
+            this.Controls.Add(this.btnGenerate);
+            this.Name = "Form1";
+            this.Text = "Sort Visualization";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.pnlCount.ResumeLayout(false);
+            this.pnlCount.PerformLayout();
+            this.ResumeLayout(false);
+        }
+
+        // In the constructor, call InitializePowerballControls to create the controls
+        public Form1()
+        {
+            InitializeComponent();
+            InitializePowerballControls();
+        }
+    }
+}
---
+++ VisualHW/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace SortVisualization
+{
+    public partial class Form1 : Form
+    {
+        private int[] mainNumbers = new int[5];
+        private int powerballNumber;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void GeneratePowerballNumbers()
+        {
+            Random random = new Random();
+            powerballNumber = random.Next(1, 70);
+
+            for (int i = 0; i < 5; i++)
+            {
+                int number;
+                do
+                {
+                    number = random.Next(1, 70);
+                } while (mainNumbers.Contains(number));
+                mainNumbers[i] = number;
+            }
+            Array.Sort(mainNumbers);
+
+            lblPowerballNumbers.Text = $"Main Numbers: {string.Join(", ", mainNumbers)}\nPowerball Number: {powerballNumber}";
+        }
+
+        private void CheckUserNumbers()
+        {
+            int[] userNumbers = textBoxUserInput.Text.Split(',').Select(int.Parse).ToArray();
+            Array.Sort(userNumbers);
+
+            bool isWinner = userNumbers.SequenceEqual(mainNumbers) && userNumbers.Contains(powerballNumber);
+
+            if (isWinner)
+            {
+                lblResult.Text = "Congratulations! You've won!";
+            }
+            else
+            {
+                lblResult.Text = "Sorry, you didn't win. Try again!";
+            }
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            GeneratePowerballNumbers();
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            CheckUserNumbers();
+        }
+    }
+}
